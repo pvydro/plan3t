9b1e041c726de5d30fc57899a2a1be6cab4e86f5
PedroDutraVaz <pedro.dutra@vizio.com>
2021-01-20 20:12:39 -0800

    Mouse movement w/ custom camera

--- src/camera/Camera.ts
+++ src/camera/Camera.ts
@@ -1,142 +1,8 @@
-// import { WindowSize, WorldSize } from '../utils/Constants'
-// import * as Viewport from 'pixi-viewport'
-// import { IUpdatable } from '../interface/IUpdatable'
-// import { InputProcessor } from '../input/InputProcessor'
-// import { IVector2, Vector2 } from '../engine/math/Vector2'
-// import { Key } from 'ts-keycode-enum'
-// import { SlowMo } from 'gsap/all'
-
-import { TARGETS } from 'pixi.js'
-import { NumericLiteral } from 'typescript'
 import { Container } from '../engine/display/Container'
+import { IVector2, Vector2 } from '../engine/math/Vector2'
+import { InputProcessor } from '../input/InputProcessor'
 import { IUpdatable } from '../interface/IUpdatable'
 import { Viewport } from './Viewport'
-
-// export interface ICamera extends IUpdatable {
-//     viewport: Viewport
-//     addChild(child: any)
-//     follow(child: any)
-// }
-
-// export interface CameraFollowOptions {
-//     followDamping: number
-// }
-
-// export class Camera implements ICamera {
-//     private static INSTANCE: Camera
-//     _viewport: Viewport
-//     _offsetEaseDamping = 5
-//     target: any
-//     targetOffset: IVector2 = Vector2.Zero
-//     offset: IVector2 = Vector2.Zero
-//     followDamping: number = 5
-
-//     public static getInstance() {
-//         if (this.INSTANCE === undefined) {
-//             this.INSTANCE = new Camera()
-//         }
-
-//         return this.INSTANCE
-//     }
-
-//     private constructor() {
-//         this._viewport = this.viewport
-
-//         // this._viewport.zoom(200)
-//         // this._viewport.zoomPercent(3)
-
-//         this.trackMousePosition()
-//         this.addKeyListeners()
-//     }
-
-//     addKeyListeners() {
-//         InputProcessor.on('keydown', (event: KeyboardEvent) => {
-//             if (event.which === Key.DownArrow) {
-//                 this.viewport.zoomPercent(-0.2)
-//             } else if (event.which === Key.UpArrow) {
-//                 this.viewport.zoomPercent(0.2)
-//             }
-//         })
-//     }
-
-//     trackMousePosition() {
-//         InputProcessor.on('mousemove', (event: MouseEvent) => {
-//             const mouseX = event.clientX
-//             const mouseY = event.clientY
-
-//             this.updateMousePosition(mouseX, mouseY)
-//         })
-//     }
-
-//     update() {
-//         // Calculate target offset with easings
-//         this.offset.x += (this.targetOffset.x - this.offset.x) / this._offsetEaseDamping
-//         this.offset.y += (this.targetOffset.y - this.offset.y) / this._offsetEaseDamping
-
-//         const projectedTargetPos = this.target ? { 
-//             x: this.target.x, 
-//             y: this.target.y 
-//         } : { x: 0, y: 0 }
-//         const targetPosWithMouseOffset = new Vector2(projectedTargetPos.x + this.offset.x, projectedTargetPos.y + this.offset.y)
-
-//         const targetX = projectedTargetPos.x//(targetPosWithMouseOffset.x - (this.viewport.width / 2)) * -1
-//         const targetY = projectedTargetPos.y//(targetPosWithMouseOffset.y - (this.viewport.width / 2)) * -1
-        
-//         const currentViewportX = this._viewport.position.x
-//         const currentViewportY = this._viewport.position.y
-        
-//         // Move viewport to target
-//         // this._viewport.position.x += (targetX - currentViewportX) / this.followDamping
-//         // this._viewport.position.y += (targetY - currentViewportY) / this.followDamping
-//         this._viewport.position.x = targetX
-//         this._viewport.position.y = targetY
-//     }
-
-//     updateMousePosition(mouseX: number, mouseY: number) {
-//         const viewportMiddleX = this.viewport.position.x + this._viewport.width / 2
-//         const viewportMiddleY = this._viewport.height / 2
-//         const offsetX = 0//(mouseX - viewportMiddleX) / 20
-//         const offsetY = 0//(mouseY - viewportMiddleY) / 15
-
-//         this.targetOffset.x = offsetX
-//         this.targetOffset.y = offsetY
-//     }
-
-//     addChild(child: any) {
-//         this._viewport.addChild(child)
-//     }
-    
-//     follow(child: any, options?: CameraFollowOptions) {
-//         this.followDamping = options ? options.followDamping : this.defaultFollowOptions.followDamping
-
-//         this.target = child
-//         // this._viewport.follow(child)
-//     }
-
-//     get viewport() {
-//         if (this._viewport === undefined) {
-//             this._viewport = new Viewport({
-//                 screenWidth: WindowSize.width,
-//                 screenHeight: WindowSize.height,
-//                 worldWidth: WorldSize.width,
-//                 worldHeight: WorldSize.height
-//             })
-
-//             this._viewport.position.set(
-//                 (0 - (this.viewport.screenWidth / 2)) * -1,
-//                 (0 - (this.viewport.screenWidth / 2)) * -1
-//             )
-//         }
-
-//         return this._viewport
-//     }
-
-//     get defaultFollowOptions() {
-//         return {
-//             followDamping: 5
-//         }
-//     }
-// }
 
 export interface ICamera extends IUpdatable {
     viewport: Viewport
@@ -148,13 +14,17 @@ export class Camera implements ICamera {
     private static INSTANCE: Camera
     private baseZoom: number = 3
     private baseWidth: number = 1280
-    private _resizeScale: number = 1
-    private _target: { x: number, y: number } = undefined
-    private _zoom: number = this.baseZoom
-    private _stage: Container
-    private _viewport: Viewport
-    private _x: number = 0
-    private _y: number = 0
+    _resizeScale: number = 1
+    _target: { x: number, y: number, width?: number, height?: number } = undefined
+    _zoom: number = this.baseZoom
+    _stage: Container
+    _viewport: Viewport
+    _x: number = 0
+    _y: number = 0
+    _offsetEaseDamping = 5
+    targetOffset: IVector2 = Vector2.Zero
+    offset: IVector2 = Vector2.Zero
+    followDamping: number = 5
 
     public static getInstance() {
         if (Camera.INSTANCE === undefined) {
@@ -165,8 +35,6 @@ export class Camera implements ICamera {
     }
 
     private constructor() {
-        // Has stage, at a fixed size, scaled by Viewport
-        // Has Viewport, which scales with screen, holding stage, device-ratio-ing it
         this._stage = new Container()
         this._viewport = new Viewport()
 
@@ -177,27 +45,46 @@ export class Camera implements ICamera {
         this.setZoom(this.baseZoom)
 
         this.viewport.addChild(this.stage)
+        this.trackMousePosition()
+    }
+
+    trackMousePosition() {
+        InputProcessor.on('mousemove', (event: MouseEvent) => {
+            const mouseX = event.clientX
+            const mouseY = event.clientY
+
+            this.updateMousePosition(mouseX, mouseY)
+        })
+    }
+
+    updateMousePosition(mouseX: number, mouseY: number) {
+        const viewportMiddleX = this.width / 2//this.x + this.width / 2
+        const viewportMiddleY = this.height / 2//this._viewport.height / 2
+        const offsetX = (mouseX - viewportMiddleX) / 20
+        const offsetY = (mouseY - viewportMiddleY) / 15
+
+        this.targetOffset.x = offsetX
+        this.targetOffset.y = offsetY
     }
 
     update() {
         if (this._target !== undefined) {
-            this.x = this._target.x
-            this.y = this._target.y
+            this.offset.x += (this.targetOffset.x - this.offset.x) / this._offsetEaseDamping
+            this.offset.y += (this.targetOffset.y - this.offset.y) / this._offsetEaseDamping
+            
+            this.x = this._target.x + this.offset.x
+            this.y = this._target.y + this.offset.y
         }
     }
 
-    follow(object: { x: number, y: number }) {
+    follow(object: { x: number, y: number, width?: number, height?: number }) {
         this._target = object
     }
 
     resize(width: number, height: number) {
-        console.log('r')
         this._resizeScale = width / this.baseWidth
 
         this.setZoom(this._zoom)
-
-        // this.viewport.width = width
-        // this.viewport.height = height
     }
 
     setZoom(amount: number) {
@@ -206,6 +93,10 @@ export class Camera implements ICamera {
         const resizeZoom = this._zoom * this._resizeScale
 
         this.stage.scale.set(resizeZoom, resizeZoom)
+    }
+
+    get target() {
+        return this._target
     }
 
     get zoom() {
