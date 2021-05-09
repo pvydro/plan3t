7c0d903e9a8274352bd553b90cd3a7d26d8e4b1b
PedroDutraVaz <pedro.dutra@vizio.com>
2021-05-09 06:00:17 -0700

    Synch w/ player xVel based on direction

--- src/camera/plugin/CameraPlayerSynchPlugin.ts
+++ src/camera/plugin/CameraPlayerSynchPlugin.ts
@@ -1,3 +1,4 @@
+import { ClientPlayer } from '../../cliententity/clientplayer/ClientPlayer'
 import { IUpdatable } from '../../interface/IUpdatable'
 import { ICamera } from '../Camera'
 
@@ -19,7 +20,13 @@ export class CameraPlayerSynchPlugin implements ICameraPlayerSynchPlugin {
 
     update() {
         if (this.target) {
+            const playerTarget = (this.target as ClientPlayer)
+            const playerDirection = (playerTarget && playerTarget.direction) ?? 1
+            
             this.targetXVelOffset = (this.target.xVel * this.xVelOffsetMultiplier)
+            this.targetXVelOffset = this.targetXVelOffset < 0 ? Math.abs(this.targetXVelOffset) : this.targetXVelOffset
+            this.targetXVelOffset *= playerDirection
+
             this.xVelOffset += (this.targetXVelOffset - this.xVelOffset) / this.xVelOffsetDivisor
 
             this.camera.extraXOffset = this.xVelOffset
